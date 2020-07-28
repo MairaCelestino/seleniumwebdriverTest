86015ec2435380da528443c0213674dbb6806e2f
MairaCelestino <mairacelestino@hotmail-com>
2020-07-28 20:30:35 +0100

    includes method for open/close the browser includes the first "Assert" command for informations validation

--- webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
+++ webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
@@ -1,12 +1,27 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.*;
 
 public class InformacoesUsuarioTest {
 	@Test
 	public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+		//Open Browser
+		System.setProperty("webdriver.chrome.driver", "/users/mairacelestino/tools/chromedriver");
+		WebDriver browser = new ChromeDriver();  
 		
+		//Open the X page in browser 
+		browser.get("http://juliodelima.com.br/taskit");
+		//Close browser
+		browser.close();
 		
+		//Validation
+		
+		assertEquals(1, 1);
 	}
 
 }
